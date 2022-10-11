00c25c2f0bc1a297351e4fdf60663c909c718fd7
edmundchan70 <edmundchan70@gmail.com>
2022-10-10 21:50:16 -0400

    commit

--- src/component/Project.js
+++ src/component/Project.js
@@ -1,8 +1,11 @@
-import React  ,{useState}from 'react'
-import "./css/Project.css"
-import {pic_project1} from "./public/config"
-import {FiArrowUpRight} from "react-icons/fi"
-import ProjectCard from "../component/ProjectCard"
+import React  ,{useState}from 'react';
+import "./css/Project.css";
+import {pic_project1} from "./public/config";
+import {FiArrowUpRight} from "react-icons/fi";
+import Pic1 from "../public/Project1_Page1.png";
+import Pic2 from "../public/Project1_Page2.png";
+import Pic3 from "../public/Project1_Page3.png";
+import ProjectCard from "../component/ProjectCard";
 function Project() {
   const id = "Project";
   const [tab , setTab] = useState(false) ; //true => 1st
@@ -15,19 +18,28 @@ function Project() {
   project1: {
     title:"Amazon Ez clone",
     Link:"https://github.com/edmundchan70/EZ_Amazon_repo",
-    Description:"This project is a clone of Amazon."
+    Description:"This project is a clone of Amazon,it is able to add ,  ",
+    Pic1:Pic1,
+    Pic2:Pic2 ,
+    Pic3:Pic3
   } , 
   project2:{
     title:"Portfolio" ,
-    Link: 
+    Link:"https://github.com/edmundchan70/Portfolio",
+    Description:"This portfolio project mostly display my front end skill",
+    Pic1:Pic1,
+    Pic2:Pic2 ,
+    Pic3:Pic3
   }
+
+
  }
  
   return (
     <section className="Project" id={id}>
             <div className='Project_Intro'>
                 <p>Project</p>
-                <t>Project ...</t>
+                <t>Project is...</t>
             </div>
             <div className="Project_Button">
                 <button className={tab? "Button_Left active" : "Button_Left"}
@@ -37,7 +49,7 @@ function Project() {
             </div>
             
             <div className="ProjectCard">
-                      <ProjectCard   />
+                      <ProjectCard  Project={tab? projects.project1 :projects.project2}/>
                 </div>
         
     </section>
--- src/component/ProjectCard.js
+++ src/component/ProjectCard.js
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import "./css/ProjectCard.css";
 
-function ProjectCard({Title, Link="https://www.google.ca/", Description="Test Project"}) {
+import Carousel from "@itseasy21/react-elastic-carousel";
+ 
+
+function ProjectCard({Project}) {
+  const carouselRef=useRef(null);
+ 
+  let resetTimeout;
+  const maxPage = 3 ; 
+  const {title , Link ,Pic1 ,Pic2 , Pic3 , Description} = Project
+  const goTo =()=>{
+    window.open(Link)
+  }
   return (
     <div className='Project_Contiainer'>
-            <div>
-                <p>{Title}</p>
-                <t>{Description}</t>
+            <div className="Project_Amazon">
+                  <p>{title}</p>
+                  <Carousel className='Carousel_comp' 
+                  
+                  ref={carouselRef}
+                
+                    
+                      onNextEnd={({ index }) => {
+                        clearTimeout(maxPage)
+                        if (index + 1 === maxPage) {
+                           resetTimeout = setTimeout(() => {
+                            carouselRef.current.goTo(0)
+                          }, 1500) // same time
+                        }
+                   }}>
+                  <div className='Carousel_Component' onClick={goTo}>
+                        <img src={Pic1}  className="Carousel_Component image"></img>
+                        <div className="Carousel_Component Link">Code</div>
+                    </div>
+                  
+                    <div className='Carousel_Component' onClick={goTo}>
+                        <img src={Pic2}  className="Carousel_Component image"></img>
+                        <div className="Carousel_Component Link">Code</div>
+                    </div>
+                   
+                    <div className='Carousel_Component' onClick={goTo}>
+                        <img src={Pic3} className="Carousel_Component image"></img>
+                        <div className="Carousel_Component Link">Code</div>
+                    </div>
+                   
+                  </Carousel>
+            
+                  <t>{Description}</t>
+            
             </div>
+            
         </div>
   )
 }
